f7120b1a445b90bffda9d967da88081607b650d6
RGM-86RF <antoniokgage@gmail.com>
2025-05-21 13:35:48 -0400

    Settings Screen with working back button and theme changer

--- app/src/main/java/com/example/pnp2_newproject/MainActivity.kt
+++ app/src/main/java/com/example/pnp2_newproject/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.example.pnp2_newproject
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.preference.PreferenceManager
 
 class MainActivity : AppCompatActivity()
 {
+
+    private lateinit var settingsBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
+       val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        when (prefs.getString("day_night", "system")){
+            "light" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            "dark" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            "system" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        }
+
+
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -18,6 +34,10 @@ class MainActivity : AppCompatActivity()
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        settingsBtn = findViewById(R.id.settings_button)
+        settingsBtn.setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
         }
     }
 }
--- app/src/main/java/com/example/pnp2_newproject/SettingsFragment.kt
+++ app/src/main/java/com/example/pnp2_newproject/SettingsFragment.kt
@@ -1,4 +1,34 @@
 package com.example.pnp2_newproject
 
-class SettingsFragment {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        val goBackPref : Preference? = findPreference("go_back")
+        goBackPref?.setOnPreferenceClickListener {
+            val intent = Intent(activity,MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
+            activity?.finish()
+            true
+        }
+
+        val themePref = findPreference<ListPreference>("day_night")
+        themePref?.setOnPreferenceChangeListener{_,newValue ->
+            when(newValue) {
+                "light" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                "dark" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                else -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            }
+            true
+        }
+    }
+
 }
