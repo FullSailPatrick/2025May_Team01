764860e84a788295e24b4c4dfbb75b0d90c332dc
Terry Revange <TRevange@student.fullsail.edu>
2025-06-11 14:57:00 -0400

    a lot of code (gesture detector, flashcard, etc

--- app/src/main/java/com/example/pnp2_newproject/GamePlayScreen.kt
+++ app/src/main/java/com/example/pnp2_newproject/GamePlayScreen.kt
@@ -1,17 +1,34 @@
 package com.example.pnp2_newproject
 
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class GamePlayScreen : AppCompatActivity()
-{
-    //code here
+//include gesture detector
+class GamePlayScreen : AppCompatActivity(), GestureDetector.OnGestureListener  {
 
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    //variables
+    private lateinit var THEFLASHCARD: androidx.cardview.widget.CardView
+    private lateinit var Question:TextView
+    private lateinit var gestureDetector: GestureDetector
+    private val swipeThreshold = 100
+    private val swipeVelocityThreshold = 100
+
+    //declare functions to load a question / answer from QuestionAnswer Class (i.e. question replaces "question here" on FlashCard)
+    fun LoadQuestion(){
+        Question.setText(QuestionAnswer.questions[2])
+    }
+    fun LoadAnswer(){
+        Question.setText(QuestionAnswer.answers[2])
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.gameplay_screen)
@@ -22,7 +39,71 @@ class GamePlayScreen : AppCompatActivity()
             insets
         }
 
-        //code here
+        // initialize the gesture detector variable
+        gestureDetector = GestureDetector(this, this)
 
+        //connect .xml id to new variables
+        THEFLASHCARD = findViewById<androidx.cardview.widget.CardView>(R.id.FlashCard)
+        Question = findViewById<TextView>(R.id.flashCardQuestion)
+
+        //call function
+        LoadQuestion()
+
+        //flip flash card
+        THEFLASHCARD.setOnClickListener()
+        {
+            THEFLASHCARD.animate()
+                .setDuration(1000)
+                .rotationYBy(360f)
+                .withEndAction {
+                    //call load answer
+                    LoadAnswer()
+                }
+        }
     }
+
+    //override this method to recognize touch event
+    override fun onTouchEvent(e: MotionEvent): Boolean {
+        return if (gestureDetector.onTouchEvent(event)) {
+            true
+        }
+        else {
+            super.onTouchEvent(event)
+        }
+    }
+
+    override fun onDown(e: MotionEvent): Boolean {
+        return false
+    }
+
+    override fun onShowPress(e: MotionEvent) {
+        return
+    }
+
+    override fun onSingleTapUp(e: MotionEvent): Boolean {
+        return false
+    }
+
+    override fun onScroll(
+        e1: MotionEvent?,
+        e2: MotionEvent,
+        distanceX: Float,
+        distanceY: Float
+    ): Boolean {
+        return false
+    }
+
+    override fun onLongPress(e: MotionEvent) {
+        return
+    }
+
+    override fun onFling(
+        e1: MotionEvent?,
+        e2: MotionEvent,
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
+        TODO("Not yet implemented")
+    }
+
 }
---
+++ app/src/main/java/com/example/pnp2_newproject/QuestionAnswer.kt
@@ -0,0 +1,19 @@
+package com.example.pnp2_newproject
+
+object QuestionAnswer {
+
+    //array of questions
+    val questions = listOf(
+        "What's 1 + 1?",
+        "What's 2 x 2?",
+        "On Feb 12, 1809 Abraham Lincoln was born. How old was his mother?"
+    )
+
+    //array of answers
+    val answers = listOf(
+        "2",
+        "4",
+        "25 years old"
+    )
+
+}
